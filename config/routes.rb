cc2a54ef211795b4e7c53ce222090fb6e9c355df
kss2002 <sc00310@naver.com>
2025-07-23 21:13:47 +0900

    resources :posts로 라우팅 통합

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,19 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/posts", to: 'posts#index'
-  get '/posts/new', to: 'posts#new'
-  get '/posts/:id', to:'posts#show'
+  resources :posts
 
+  # // resources :posts는 아래의 모든 라우팅을 자동으로 생성하는 명령어
+
+  #get "/posts", to: 'posts#index'
+  #get '/posts/new', to: 'posts#new'
+  #get '/posts/:id', to:'posts#show'
+  #post '/posts', to: 'posts#create'
+  #get '/posts/:id/edit', to: 'posts#edit'
+  #patch '/posts/:id', to: 'posts#update'
+  #delete '/posts/:id', to: 'posts#destroy'
+  
+  # ---------------
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
